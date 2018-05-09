0a3c6bde4e49b9207454d96d38b66f73df65f474
Ryan Groesch <ryan.groesch@gmail.com>
2018-05-09 16:57:01 -0700

    added TODO's and testing documents

--- src/lib/traversals.js
+++ src/lib/traversals.js
@@ -2,35 +2,29 @@
 
 // Time - O(N) where n is the number of nodes
 // Space - O(H) where h is the height of the tree
+// Traversal path Root -> Left -> Right
 const preOrderTraversal = (rootNode) => {
-// Vinicio - Root - Left - Right
   if (!rootNode) {
     return undefined;
   }
-  //---------------------------------------------------------
-  // Vinicio - Here, I AM IN ROOT
   console.log(`Visiting a node with value ${rootNode.value}`);
-  //---------------------------------------------------------
-  preOrderTraversal(rootNode.left); // rootNode.left is a sub-tree
+  
+  preOrderTraversal(rootNode.left);  
   preOrderTraversal(rootNode.right);
   return undefined;
 };
 
 // Time - O(H) where n is the number of nodes
 // Space - O(H) where h is the height of the tree
+// Traversal path Left -> Right -> Root
 const postOrderTraversal = (rootNode) => {
-// Vinicio -  Left - Right -Root
   if (!rootNode) {
     return undefined;
   }
   postOrderTraversal(rootNode.left);
   postOrderTraversal(rootNode.right);
-  //---------------------------------------------------------
-  // This line is not going to happen until we visit ALL left and ALL right
-  // Vinicio - Here, I AM IN ROOT
   console.log(`Visiting a node with value ${rootNode.value}`);
   return undefined;
-  //---------------------------------------------------------
 };
 
-export { preOrderTraversal,postOrderTraversal };
+export { preOrderTraversal, postOrderTraversal };
