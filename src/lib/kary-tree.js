23c9405afb7a10c6574a38023b48a3fc6c3fee73
Ryan Groesch <ryan.groesch@gmail.com>
2018-05-14 08:48:31 -0700

    Stuff for k-ary

--- src/lib/kary-tree.js
+++ src/lib/kary-tree.js
@@ -34,9 +34,62 @@ export default class KAryTree {
   }
   // TODO: Find(value) iterate through using a breadth first approach return first value, 
   // if not included return null
+  // Time : O(H) -> O(lg n)
+  // Space : O(H) -> O(lg n)
+  find(value) {
+    if (!this.root) {
+      return null;
+    }
+    return this._find(this.root, value);
+  }
+
+  _find(rootNode, value) {
+    const queue = new Queue();
+    queue.enqueue(rootNode);
+
+    let currentNode = null;
+
+    while (!queue.isEmpty()) {
+      currentNode = queue.dequeue();
+
+      if (currentNode.value === value) {
+        return currentNode;
+      }
+      console.log(`Visiting ${currentNode.value}`);
+      for (let i = 0; i < currentNode.children.length; i++) {
+        queue.enqueue(currentNode.children[i]);
+      }
+    }
+  }
 
   // TODO: toString(string) iterate over all nodes breadthfirst and 
   // concatenate into a string with new lines
+  toString() {
+    if (!this.root) {
+      return null;
+    }
+    let ansString = '';
+    return this._toString(this.root);
+  }
 
+  _toString(rootNode) {
+    const queue = new Queue();
+    queue.enqueue(rootNode);
+
+    let currentNode = null;
+
+    while (!queue.isEmpty()) {
+      currentNode = queue.dequeue();
+
+      ansString += `${currentNode.value}/n`;
+      for (let i = 0; i < currentNode.children.length; i++) {
+        queue.enqueue(currentNode.children[i]);
+      }
+    }
+  }
   // TODO: toArray(array) depth first traversal push all elements into an array. 
+  // utilize a stack and queue to 
+  toArray(array) {
+    
+  }
 }
